58f173414e9930f3453148f93910ffde8331e9dd
emmielewis <emmie_lewis@hotmail.com>
2016-03-06 15:58:11 -0500

    Initial Check in

---
+++ RenewCert/stdafx.h
@@ -0,0 +1,21 @@
+// stdafx.h : include file for standard system include files,
+// or project specific include files that are used frequently, but
+// are changed infrequently
+//
+
+// stdafx.h : include file for standard system include files,
+// or project specific include files that are used frequently, but
+// are changed infrequently.
+//
+
+#pragma once
+
+
+#define WIN32_LEAN_AND_MEAN                     // Exclude rarely used material from Windows headers.
+#include <stdio.h>
+#include <tchar.h>
+#include <windows.h>
+#include <wincrypt.h>
+
+
+// TODO: reference additional headers your program requires here
